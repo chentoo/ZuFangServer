af3e3aed2a34ebc1df22d0b6c85c5b8189a1bae7
chentoo <chentoo@gmail.com>
2014-05-21 18:00:33 +0800

    爬虫1.0 上线

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,126 @@
 require("cloud/app.js");
-// Use AV.Cloud.define to define as many cloud functions as you want.
-// For example:
+var House = AV.Object.extend("House");
+
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+if(__production)
+{
+	AV.Cloud.setInterval("saveBaxingAds", 30, function(){
+		try
+		{
+			AV.Cloud.httpRequest({
+				url: 'http://www.douban.com/group/shanghaizufang/discussion',
+				params: 'start=0',
+				headers: {
+				    'User-Agent':'Mozilla/5.0 (Linux; U; Android 2.2.1; zh-cn; HTC_Wildfire_A3333 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
+				},
+				success: function(httpResponse) {
+					// console.log(httpResponse.text);
+					parseHtml(httpResponse.text);
+				},
+				error: function(httpResponse) {
+					console.error('Request failed with response code ' + httpResponse.status);
+				}
+			});
+		} catch (err) {
+
+		}
+	});
+}
+
+function parseHtml(html)
+{
+	var indexOfTr = html.indexOf('<tr class="">');
+	if (indexOfTr == -1) {
+		return;
+	}
+	var step1text = html.substring(indexOfTr + 14);
+
+	var indexOfTrEnd = step1text.indexOf('</tr>');
+	var step2text = step1text.substring(0, indexOfTrEnd);
+
+	var otherText = step1text.substring(indexOfTrEnd);
+
+	parseHouseDate(step2text);
+	parseHtml(otherText);
+}
+
+function parseHouseDate(houseDate)
+{
+	var url = stringWithStartEnd(houseDate, '<a href="', '" title=');
+
+	var title = stringWithStartEnd(houseDate, 'title="', '" class=');
+
+	houseDate = houseDate.substring(houseDate.indexOf(title) + title.length);
+	var userUrl = stringWithStartEnd(houseDate, 'nowrap="nowrap"><a href="', '" class="">');
+	houseDate = houseDate.substring(houseDate.indexOf(userUrl) + userUrl.length);
+
+	var userNickname = stringWithStartEnd(houseDate , '" class="">', '</a>');
+	var commentCount = stringWithStartEnd(houseDate , 'nowrap" class="">', '</td>');
+	var updateTime 	 = stringWithStartEnd(houseDate , 'class="time">', '</td>');
+
+	console.log(url + '\ntitle:' + title + '\nuserUrl:' + userUrl + '\nnicekName:' + userNickname + '\ncommentCount:' + commentCount + '\nupdateTime:' + updateTime);
+	saveToAVOS(url, title, userUrl, userNickname, commentCount, updateTime);
+}
+
+function stringWithStartEnd(baseString, startString, endString)
+{
+	var indexOfStart = baseString.indexOf(startString);
+	var indexOfEnd = baseString.substring(indexOfStart + startString.length).indexOf(endString);
+	var result = baseString.substring(indexOfStart + startString.length, indexOfStart + startString.length + indexOfEnd);
+	return result;
+}
+
+function saveToAVOS(houseUrl, title, userUrl, userNickname, commentCount, updateTime)
+{
+	try
+	{
+		var houseQuery = new AV.Query("House");
+		houseQuery.equalTo("houseUrl",houseUrl);
+		houseQuery.find({
+			success: function(houses) {
+
+			  	if(houses.length > 0)//house 存在
+			  	{
+
+				}
+			  	else
+			  	{
+					var house = new House();
+
+					house.set("houseUrl", houseUrl);
+					house.set("title", title);
+					house.set("userUrl", userUrl);
+					house.set("userNickname", userNickname);
+					house.set("commentCount", commentCount);
+					house.set("updateTime", updateTime);
+
+					house.save(null, {
+					success: function(thisHouse) {
+					  	console.log("-----【处理存储数据】-----【存储House】-----✓✓✓ 保存成功:" + houseUrl);
+					},
+					error: function(thisHouse, error) {
+					  	console.log("-----【处理存储数据】-----【存储House】-----XXX 保存失败" + error)
+					}
+				});
+			  	}
+			},
+			error:function(error) {
+				response.send(500);
+			}
+		});
+	}catch (err) {
+		console.dir(err);
+	}
+
+}
+
+
+
+
+
+
+
+ 
